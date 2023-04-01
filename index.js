2a05a4fd979fd02d311ebcf287c81c306d220c83
Samuel Gichuhi <gichuhi1sam@gmail.com>
2023-04-02 00:05:23 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+
+function submitData(userName,userEmail){
+ 
+const object={ name: userName, email: userEmail,}
+
+const config={
+    method: "POST",
+    headers: {
+        "Content-Type" : "application/json",
+        Accept : "application/json",
+    },
+    body: JSON.stringify(object),
+}
+
+   return fetch("http://localhost:3000/users",config)
+   .then((resp => resp.json()))
+      .then((data => document.body.innerHTML+=(`<p>${data.id}</p>`)))
+         .catch((error => document.body.innerHTML+=(`<p>${error.message}</p>`)))
+
+}
+
+
+console.log(submitData("Samuel","Email"));
+
+
